f1c010c16784297c0e32d24f480b74ee753dceaa
Jonathan Donaldson <jdonaldson278@gmail.com>
2021-03-12 02:05:51 +0000

    Updated to output correct format

--- src/routes/pokemon.rs
+++ src/routes/pokemon.rs
@@ -1,7 +1,9 @@
 use crate::client::client_error;
 use crate::client::pokemon_client::get_description;
 use crate::{POKEMON_CLIENT, SHAKESPEARE_CLIENT};
+use serde::{Deserialize, Serialize};
 use std::convert::Infallible;
+
 use warp::Filter;
 
 pub fn pokemon_filter() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone
@@ -9,6 +11,16 @@ pub fn pokemon_filter() -> impl Filter<Extract = impl warp::Reply, Error = warp:
     warp::path!("pokemon" / String)
         .and(warp::get())
         .and_then(get_pokemon)
+}
+#[derive(Serialize, Deserialize)]
+pub struct GetPokemonOutput {
+    name: String,
+    description: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GetPokemonErrorOutput {
+    error: String,
 }
 
 async fn get_pokemon(pokemon_name: String) -> Result<impl warp::Reply, Infallible> {
@@ -20,20 +32,28 @@ async fn get_pokemon(pokemon_name: String) -> Result<impl warp::Reply, Infallibl
                 let translation_response = SHAKESPEARE_CLIENT.get_translation(&description).await;
                 return match translation_response {
                     Ok(translation) => {
+                        let output = GetPokemonOutput {
+                            name: pokemon_name,
+                            description: translation,
+                        };
                         return Ok(warp::reply::with_status(
-                            translation,
+                            warp::reply::json(&output),
                             warp::http::StatusCode::OK,
                         ));
                     }
                     Err(err) => match err {
                         client_error::ClientError::ShakespeareTooManyRequestsError => {
                             Ok(warp::reply::with_status(
-                                "Too many requests".into(),
+                                warp::reply::json(&GetPokemonErrorOutput {
+                                    error: "Too many requests".into(),
+                                }),
                                 warp::http::StatusCode::TOO_MANY_REQUESTS,
                             ))
                         }
                         _ => Ok(warp::reply::with_status(
-                            "Failed to get shakespeare translation".into(),
+                            warp::reply::json(&GetPokemonErrorOutput {
+                                error: "Failed to get shakespeare translation".into(),
+                            }),
                             warp::http::StatusCode::INTERNAL_SERVER_ERROR,
                         )),
                     },
@@ -41,17 +61,23 @@ async fn get_pokemon(pokemon_name: String) -> Result<impl warp::Reply, Infallibl
             }
 
             Ok(warp::reply::with_status(
-                "Failed to find pokemon description".into(),
+                warp::reply::json(&GetPokemonErrorOutput {
+                    error: "Failed to find pokemon description".into(),
+                }),
                 warp::http::StatusCode::NOT_FOUND,
             ))
         }
         Err(pokemon_error) => match pokemon_error {
             client_error::ClientError::PokemonNotFoundError => Ok(warp::reply::with_status(
-                "Failed to find pokemon".into(),
+                warp::reply::json(&GetPokemonErrorOutput {
+                    error: "Failed to find pokemon".into(),
+                }),
                 warp::http::StatusCode::NOT_FOUND,
             )),
             _ => Ok(warp::reply::with_status(
-                "Failed to get pokemon".into(),
+                warp::reply::json(&GetPokemonErrorOutput {
+                    error: "Failed to get pokemon".into(),
+                }),
                 warp::http::StatusCode::INTERNAL_SERVER_ERROR,
             )),
         },
@@ -67,7 +93,6 @@ mod tests {
         pokemon_client::{FlavorTextEntry, Language, Pokemon},
         shakespeare_client::{ShakespeareResponse, ShakespeareSuccess, ShakespeareTextContents},
     };
-    use serde::{Deserialize, Serialize};
     use serde_json::json;
 
     use wiremock::matchers::{body_json, method, path};
@@ -137,6 +162,85 @@ mod tests {
 
         // assert
         assert_eq!(res.status(), 200);
-        assert_eq!(res.body(), "Spits fire yond is hot enow to melt boulders. Known to cause forest fires unintentionally.");
+        assert_eq!(res.body(), "{\"name\":\"charizard\",\"description\":\"Spits fire yond is hot enow to melt boulders. Known to cause forest fires unintentionally.\"}");
+    }
+
+	#[tokio::test]
+    async fn it_returns_404_on_invalid_pokemon() {
+        // arrange
+        let mock_server = MockServer::start().await;
+        env::set_var("port", "5000");
+        env::set_var("pokemon_api_base_url", mock_server.uri());
+        env::set_var("shakespeare_api_base_url", mock_server.uri());
+
+        Mock::given(method("GET"))
+            .and(path("/api/v2/pokemon-species/invalidPokemon"))
+            .respond_with(ResponseTemplate::new(404))
+            .mount(&mock_server)
+            .await;
+
+        // acct
+        let filter = pokemon_filter();
+        let res = warp::test::request()
+            .method("GET")
+            .path("/pokemon/invalidPokemon")
+            .reply(&filter)
+            .await;
+
+        // assert
+        assert_eq!(res.status(), 404);
+        assert_eq!(res.body(), "{\"error\":\"Failed to find pokemon\"}");
+    }
+
+	#[tokio::test]
+    async fn it_returns_429_on_too_many_shakespeare_requests() {
+       // arrange
+	   let mock_server = MockServer::start().await;
+	   env::set_var("port", "5000");
+	   env::set_var("pokemon_api_base_url", mock_server.uri());
+	   env::set_var("shakespeare_api_base_url", mock_server.uri());
+
+	   let expected_text = TextInput {
+		   text: "Spits fire that is hot enough to melt boulders. Known to cause forest fires unintentionally.".into(),
+	   };
+
+	   let generated_pokemon = Pokemon {
+		   id: 6,
+		   name: "charizard".into(),
+		   flavor_text_entries: vec![FlavorTextEntry {
+			   flavor_text: "Spits fire that is hot enough to melt boulders.\nKnown to cause forest fires unintentionally.".into(),
+			   language: Language {
+				   name: "en".into(),
+				   url: "https://pokeapi.co/api/v2/language/9/".into()
+			   },
+		   }],
+	   };
+
+	   let mock_pokemon_response =
+		   ResponseTemplate::new(200).set_body_json(json!(generated_pokemon));
+
+	   Mock::given(method("POST"))
+		   .and(body_json(expected_text))
+		   .respond_with(ResponseTemplate::new(429))
+		   .mount(&mock_server)
+		   .await;
+
+	   Mock::given(method("GET"))
+		   .and(path("/api/v2/pokemon-species/charizard"))
+		   .respond_with(mock_pokemon_response)
+		   .mount(&mock_server)
+		   .await;
+
+	   // acct
+	   let filter = pokemon_filter();
+	   let res = warp::test::request()
+		   .method("GET")
+		   .path("/pokemon/charizard")
+		   .reply(&filter)
+		   .await;
+
+	   // assert
+	   assert_eq!(res.status(), 429);
+	   assert_eq!(res.body(), "{\"error\":\"Too many requests\"}");
     }
 }
